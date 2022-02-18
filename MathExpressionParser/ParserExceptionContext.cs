2c8b7e7da2721142a0bdc7358d0a364f7e2a52c1
palapapa <palapapa93@gmail.com>
2022-02-19 06:53:00 +0800

    Renamed ParserExceptionContext.ErrorPosition to Position

--- MathExpressionParser/ParserExceptionContext.cs
+++ MathExpressionParser/ParserExceptionContext.cs
@@ -3,14 +3,14 @@
 /// <summary>
 /// A record class that contains additional information regarding a <see cref="ParserException"/> instance.
 /// </summary>
-/// <param name="ErrorPosition">The position in the <see cref="MathExpression"/> where the <see cref="ParserException"/> occurs.</param>
+/// <param name="Position">The position in the <see cref="MathExpression"/> where the <see cref="ParserException"/> occurs.</param>
 /// <param name="Type">The type of error that happened.</param>
-public record class ParserExceptionContext(int ErrorPosition, ParserExceptionType Type)
+public record class ParserExceptionContext(int Position, ParserExceptionType Type)
 {
     /// <summary>
     /// The position in the <see cref="MathExpression"/> where the <see cref="ParserException"/> occurs.
     /// </summary>
-    public int ErrorPosition { get; init; } = ErrorPosition;
+    public int Position { get; init; } = Position;
 
     /// <summary>
     /// The kind of error that caused a <see cref="ParserException"/> to be thrown.
