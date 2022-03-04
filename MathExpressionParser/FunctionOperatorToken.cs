d13031fe403d7e695ff870c95d0a2e29ddc7759e
palapapa <palapapa93@gmail.com>
2022-03-04 16:44:16 +0800

    Added token-related record classes
    Added token-related record classes that are used to annotate the purpose
    of a token in a math expression.
    
    Renamed FunctionToken to FunctionOperatorToken.

---
+++ MathExpressionParser/FunctionOperatorToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathExpressionParser;
+
+internal record class FunctionOperatorToken : Token
+{
+    public int ArgumentCount { get; init; }
+
+    public FunctionOperatorToken(Token token, int argumentCount) : base(token)
+    {
+        ArgumentCount = argumentCount;
+    }
+}
---
+++ MathExpressionParser/PostfixUnaryOperatorToken.cs
@@ -0,0 +1,8 @@
+﻿namespace MathExpressionParser;
+
+internal record class PostfixUnaryOperatorToken : Token
+{
+    public PostfixUnaryOperatorToken(Token token) : base(token)
+    {
+    }
+}
